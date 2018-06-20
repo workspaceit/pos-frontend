fbf76fda42bd0f2384bf8189df3e1d54afe87f84
wsitrafi <rafi@workspaceit.com>
2018-06-20 19:07:38 +0600

    Profit and loss continued

--- src/app/admin/accounts/components/profit-and-loss/profit-and-loss.component.ts
+++ src/app/admin/accounts/components/profit-and-loss/profit-and-loss.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../../../services/report.service';
 import {ProfitAndLossReport} from '../../../../models/data/report/profit-and-loss-report';
+import {ReportAccount} from '../../../../models/data/report/report-account';
 
 @Component({
   selector: 'app-profit-and-loss',
@@ -9,19 +10,34 @@ import {ProfitAndLossReport} from '../../../../models/data/report/profit-and-los
   providers:[CategoryService]
 })
 export class ProfitAndLossComponent implements OnInit {
+  protected tableRows:ReportAccount[];
   protected profitAndLossReport:ProfitAndLossReport;
 
   constructor(private categoryService:CategoryService) {
+    this.tableRows = [];
    this.profitAndLossReport = new ProfitAndLossReport();
   }
 
   private getProfitAndLoss(){
-    this.categoryService.getProfitandLoss('2014-02-02','2018-08-12').subscribe(value => {
+    this.categoryService.getProfitAndLoss('2014-02-02','2018-08-12').subscribe(value => {
         this.profitAndLossReport = value;
+        this.tableRows =  this.getRows( this.profitAndLossReport.expenseAccounts);
+
+
     });
   }
   ngOnInit() {
     this.getProfitAndLoss();
   }
+  private getRows(reportAccounts:ReportAccount[]):ReportAccount[]{
+    const tableRows: ReportAccount[] = [];
+    for(const ra of reportAccounts){
+      tableRows.push(ra);
 
+      const tmpTableRows =  this.getRows(ra.child);
+      this.tableRows = tableRows.concat(tmpTableRows );
+    }
+    console.log("tableRows",tableRows);
+    return tableRows;
+  }
 }
